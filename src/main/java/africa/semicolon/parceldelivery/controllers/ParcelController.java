2cf363b8db7cb236b928168f520cdb1dd0645948
apimpnamedodogwu <edenelenwoke@gmail.com>
2022-08-21 15:48:51 +0100

    Parcel Implementation

--- src/main/java/africa/semicolon/parceldelivery/controllers/ParcelController.java
+++ src/main/java/africa/semicolon/parceldelivery/controllers/ParcelController.java
@@ -1,5 +1,7 @@
 package africa.semicolon.parceldelivery.controllers;
 
+import africa.semicolon.parceldelivery.models.Location;
+import africa.semicolon.parceldelivery.requests.ParcelCreationRequest;
 import africa.semicolon.parceldelivery.services.ParcelService;
 
 
@@ -59,5 +61,22 @@ public class ParcelController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+    @PostMapping("/create-parcel/request")
+    public ResponseEntity<?> createParcel(@RequestBody ParcelCreationRequest request) {
+        parcelService.createParcel(request);
+        var body = "Your parcel has been created successfully";
+        return new ResponseEntity<>(body, HttpStatus.CREATED);
+    }
 
+    @GetMapping("/get-parcel-details/{parcel_id}")
+    public ResponseEntity<?> getParcelDetails(@PathVariable Long parcel_id) {
+        parcelService.getParcelDetails(parcel_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/update-parcel-location/{parcel_id}/new-location")
+    public ResponseEntity<?> updateParcelLocation(@PathVariable Long parcel_id, @RequestBody Location location) {
+        parcelService.updateParcelLocation(parcel_id, location);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }
