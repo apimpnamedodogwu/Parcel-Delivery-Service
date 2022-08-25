92a363dece49bbb43c6124d03701910230ad27f1
apimpnamedodogwu <edenelenwoke@gmail.com>
2022-08-25 09:40:53 +0100

    User Repository Test

--- src/main/java/africa/semicolon/parceldelivery/models/User.java
+++ src/main/java/africa/semicolon/parceldelivery/models/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements Serializable {
 
     @javax.persistence.Id
--- src/test/java/africa/semicolon/parceldelivery/repositories/UserRepositoryTest.java
+++ src/test/java/africa/semicolon/parceldelivery/repositories/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,10 +16,12 @@ class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
     User user;
+    User userTwo;
 
     @BeforeEach
     void setUp() {
         user = new User();
+        userTwo = new User();
     }
 
     @Test
@@ -30,16 +33,42 @@ class UserRepositoryTest {
         user.setEmail("eden.kwinesta@gamil.com");
         user.setRole(Role.TYPE_1);
         var savedUser = userRepository.save(user);
-        var email = userRepository.findUserByEmail(savedUser.getEmail());
-        assertThat(email.isPresent()).isTrue();
-        assertThat(email.get().getEmail()).isEqualTo(savedUser.getEmail());
+        var existingUser = userRepository.findUserByEmail(savedUser.getEmail());
+        assertThat(existingUser.isPresent()).isTrue();
+        assertThat(existingUser.get().getEmail()).isEqualTo(savedUser.getEmail());
     }
 
     @Test
-    void findUserById() {
+    void testThatUserCanBeFoundById() {
+        user.setFirstName("Eden");
+        user.setLastName("Elenwoke");
+        user.setPassword("12345678");
+        user.setUserName("edentheheathen");
+        user.setEmail("eden.kwinesta@gamil.com");
+        user.setRole(Role.TYPE_1);
+        var savedUser = userRepository.save(user);
+        var existingUser = userRepository.findUserById(savedUser.getId());
+        assertThat(existingUser.isPresent()).isTrue();
+        assertThat(existingUser.get().getId()).isEqualTo(savedUser.getId());
     }
 
     @Test
-    void findAllBy() {
+    void testThatAllUsersCanBeFound() {
+        user.setFirstName("Eden");
+        user.setLastName("Elenwoke");
+        user.setPassword("12345678");
+        user.setUserName("edentheheathen");
+        user.setEmail("eden.kwinesta@gamil.com");
+        user.setRole(Role.TYPE_1);
+        userRepository.save(user);
+        userTwo.setFirstName("Dorcas");
+        userTwo.setLastName("Abang");
+        userTwo.setUserName("dollydee");
+        userTwo.setPassword("12345678");
+        userTwo.setEmail("adeweh@gmail.com");
+        userTwo.setRole(Role.TYPE_1);
+        userRepository.save(userTwo);
+        var list = userRepository.findAllBy(Pageable.ofSize(20));
+        assertThat(list.size()).isEqualTo(2);
     }
 }
